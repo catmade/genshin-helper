22000f85350196635df6e12dcba03c7e65664a8c
catmade <catmade@qq.com>
2022-02-27 01:19:45 +0800

    feat: start

---
+++ ClickArea.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace genshin_helper
+{
+    public partial class ClickArea : Window
+    {
+        ///win32 api
+        private const int WS_EX_TRANSPARENT = 0x20;
+
+        private const int GWL_EXSTYLE = (-20);
+        private bool _mousePassThrough;
+        private double _opacity;
+        private int _radius;
+
+        public ClickArea() : this(50, 0.5)
+        {
+        }
+
+        public ClickArea(int radius, double opacity)
+        {
+            InitializeComponent();
+            DataContext = this;
+            Radius = radius;
+        }
+
+        public int Radius
+        {
+            get => _radius;
+            set
+            {
+                if (value == _radius) return;
+                _radius = value;
+                this.Height = _radius;
+                this.Width = _radius;
+                this.border.CornerRadius = new CornerRadius
+                {
+                    TopLeft = _radius,
+                    TopRight = _radius,
+                    BottomLeft = _radius,
+                    BottomRight = _radius,
+                };
+            }
+        }
+
+        public bool MousePassThrough
+        {
+            get => _mousePassThrough;
+            set
+            {
+                if (_mousePassThrough == value) return;
+                _mousePassThrough = value;
+                if (_mousePassThrough) enableMousePassThrough();
+                else disableMousePassThrough();
+            }
+        }
+
+        public new double Opacity
+        {
+            get => _opacity;
+            set
+            {
+                if (_opacity == value) return;
+                _opacity = value;
+                this.border.Opacity = _opacity;
+            }
+        }
+
+        private void enableMousePassThrough()
+        {
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            uint extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle | WS_EX_TRANSPARENT);
+        }
+
+        private void disableMousePassThrough()
+        {
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            uint extendedStyle = GetWindowLong(hwnd, GWL_EXSTYLE);
+            SetWindowLong(hwnd, GWL_EXSTYLE, extendedStyle ^ WS_EX_TRANSPARENT);
+        }
+
+        [DllImport("user32", EntryPoint = "SetWindowLong")]
+        private static extern uint SetWindowLong(IntPtr hwnd, int nIndex, uint dwNewLong);
+
+        [DllImport("user32", EntryPoint = "GetWindowLong")]
+        private static extern uint GetWindowLong(IntPtr hwnd, int nIndex);
+
+        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (e.LeftButton == System.Windows.Input.MouseButtonState.Pressed) this.DragMove();
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace genshin_helper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private ClickArea clickArea;
+
+        public MainWindow()
+        {
+            this.clickArea = new ClickArea
+            {
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            };
+            clickArea.Show();
+
+            InitializeComponent();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            clickArea.Close();
+            base.OnClosing(e);
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            clickArea.MousePassThrough = (bool) ((CheckBox) sender).IsChecked;
+        }
+
+        private void SliderOpacity_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = ((Slider) sender);
+            clickArea.Opacity = slider.Value / slider.Maximum;
+        }
+
+        private void SliderRadius_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = (Slider) sender;
+            clickArea.Radius = (int) slider.Value;
+        }
+    }
+}
